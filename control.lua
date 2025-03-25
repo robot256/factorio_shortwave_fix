fd061c7ea49b0f877fa812111bb99ec441986cb2
robot256 <robot256@GMAIL.COM>
2025-03-25 14:38:24 -0700

    New channel naming logic
    - Channel name now uses Group Name instead of signals, if one is set.
    - Channel name now uses all slots in group, including empty spaces between signals.
    - Channel name now uses quality on all signals.
    - Channel name and radio connections are checked and regenerated in on_configuration_changed in case signals/qualities are removed. Migration warning is printed to console and log file when this happens.

--- control.lua
+++ control.lua
@@ -1,15 +1,19 @@
 local math2d = require("math2d")
 
+-- Creates a table entry for this force if necessary
 local function check_state(force)
   if not storage[force.index] then
     storage[force.index] = {}
   end
 end
 
+-- Returns the channel string corresponding with the current combinator state
 local function get_channel_string(radio, toggle)
   local cb = radio.get_control_behavior()
+  
+  -- Make sure there is always exactly one section
   if cb.sections_count == 0 then
-    cb.add_section()  -- Make sure there is always one section
+    cb.add_section()
   else
     while cb.sections_count > 1 do
       cb.remove_section(2)
@@ -22,25 +26,47 @@ local function get_channel_string(radio, toggle)
     return
   end
   
+  -- Make sure that the first section was created correctly
   local section = cb.get_section(1)
   if not section then
     return
   end
   
-  local channel_slot = section.get_slot(1)
-  if not channel_slot or not channel_slot.value then
+  -- Make sure the first section always stays active. Disable the radio with the constant combinator enable switch.
+  if not section.active then
+    section.active = true
+  end
+  
+  -- If the first section is named (in a group), use the group name as the channel instead of the icons
+  if section.group and section.group ~= "" then
+    return "group."..section.group
+  end
+  
+  -- Make sure that at least one signal entry is in the section
+  if section.filters_count == 0 then
     return
   end
   
-  local channel_string = channel_slot.value.name..":"..channel_slot.min
+  -- Assemble the channel string using all specified signals and empty slots
+  local channel_string = ""
+  for i=1,section.filters_count do
+    if i > 1 then
+      channel_string = channel_string .. ","
+    end
+    local channel_slot = section.get_slot(i)
+    if channel_slot and channel_slot.value then
+      channel_string = channel_string .. channel_slot.value.name.."."..channel_slot.value.quality.."."..channel_slot.min
+    end
+  end
   return channel_string
 end
 
+-- Checks for channel links with no radios connected anymore, and deletes them
 local function check_channels()
   for team,channels in pairs(storage) do
     for channel,link in pairs(channels) do
       if not link or not link.valid then
-        log("Shortwave origin link became invalid for channel "..tostring(team).."."..channel..", channel removed.")
+        log("Shortwave origin link became invalid for channel \""..(game.forces[team] and game.forces[team].name or tostring(team)).."::"..channel.."\", channel removed.")
         channels[channel] = nil
       else
         local link_red = link.get_wire_connector(defines.wire_connector_id.circuit_red)
@@ -54,6 +80,7 @@ local function check_channels()
   end
 end
 
+-- Locates or creates the shortwave-link entity associated with this radio
 local function radio_link(radio)
   local links = radio.surface.find_entities_filtered{
     name = "shortwave-link",
@@ -75,6 +102,7 @@ local function radio_link(radio)
   return link
 end
 
+-- Locates or creates the shortwave-port entity associated with this radio
 local function radio_port(radio)
   local ports = radio.surface.find_entities_filtered{
     name = "shortwave-port",
@@ -111,10 +139,50 @@ local function radio_port(radio)
   return port
 end
 
-local function radio_tune(radio, toggle)
+-- Tries to find the channel this radio is currently connected to
+local function get_existing_channel(link)
+  local team = link.force.index
+  if not storage[team] then return end
+  
+  -- Find the central link this radio-link is connected to
+  local target_relay
+  local link_red = link.get_wire_connector(defines.wire_connector_id.circuit_red)
+  for _, connection in pairs(link_red.connections) do
+    if connection.target.owner.name == "shortwave-link" then
+      target_relay = connection.target.owner
+      break
+    end
+  end
+  
+  -- Find the channel this central link is associated with
+  if not target_relay then return end
+  for channel, relay in pairs(storage[team]) do
+    if relay == target_relay then
+      return channel
+    end
+  end
+end
+
+-- Reads the radio tuning settings and connects invisible wires as needed
+local function radio_tune(radio, toggle, showlog)
   local team = radio.force.index
   local link = radio_link(radio)
   local port = radio_port(radio)
+  
+  -- Check if the channel name changed and log if it did
+  local old_channel = get_existing_channel(link)
+  local channel = get_channel_string(radio, toggle)
+  
+  if old_channel and channel and old_channel == channel then
+    -- New and old channel are identical, nothing to do here
+    return false
+  end
+  
+  if showlog then
+    log("Retuning "..tostring(radio)..": \""..(old_channel and (game.forces[team] and game.forces[team].name or tostring(team)).."::"..old_channel or "<disabled>").."\" became \""..(channel and (game.forces[team] and game.forces[team].name or tostring(team)).."::"..channel or "<disabled>").."\"")
+  end
+  
+  -- Channel changed, remove old wires if any
   local link_red = link.get_wire_connector(defines.wire_connector_id.circuit_red)
   local link_green = link.get_wire_connector(defines.wire_connector_id.circuit_green)
 
@@ -130,12 +198,13 @@ local function radio_tune(radio, toggle)
     end
   end
 
-  local channel = get_channel_string(radio, toggle)
+  -- Check if new channel does not exist (radio disabled)
   if not channel then
     --game.print("Radio disabled")
-    return
+    return true
   end
 
+  -- Make central link for new channel if necessary
   if not storage[team][channel] then
     storage[team][channel] = radio.surface.create_entity{
       name = "shortwave-link",
@@ -144,6 +213,7 @@ local function radio_tune(radio, toggle)
     }
   end
 
+  -- Connect wires to new relay
   local relay = storage[team][channel]
   local relay_red = relay.get_wire_connector(defines.wire_connector_id.circuit_red)
   local relay_green = relay.get_wire_connector(defines.wire_connector_id.circuit_green)
@@ -158,6 +228,7 @@ local function radio_tune(radio, toggle)
   link_green.connect_to(port_green, false, defines.wire_origin.script)
 
   --game.print("Radio tuned")
+  return true
 end
 
 local function OnEntityCreated(event)
@@ -291,16 +362,36 @@ script.on_event(defines.events.on_player_pipette, function(event)
   end
 end)
 
--- No work on startup
+-- No work on startup or install
 --script.on_init(function()
 --end)
 
 --script.on_load(function()
 --end)
 
+-- On configuration changed, need to recheck that all the channel signals still exist
+script.on_configuration_changed(function()
+  local warning = false
+  local old_storage = serpent.block(storage)
+  
+  for _,surface in pairs(game.surfaces) do
+    for _,entity in pairs(surface.find_entities_filtered{name="shortwave-radio"}) do
+      -- Log entry for every radio channel that changed due to migration
+      warning = radio_tune(entity, false, true) or warning
+    end
+  end
+  check_channels()
+  
+  if warning then
+    game.print{"shortwave-message.migration-changed"}
+    log{"shortwave-message.migration-log",old_storage,serpent.block(storage)}
+  end
+end)
+
 
 -- Console commands
 commands.add_command("shortwave-dump", "Dump storage to log", function() log(serpent.block(storage)) end)
+commands.add_command("shortwave-debug", "Dump storage to console", function() game.print(serpent.block(storage)) end)
 
 ------------------------------------------------------------------------------------
 --                    FIND LOCAL VARIABLES THAT ARE USED GLOBALLY                 --
